5d6852b62dac929c453a77c3168c2a4f32abb862
Adrian <45540128+averanes@users.noreply.github.com>
2021-10-05 12:50:40 +0200

    Initial commit Private conversations working OK

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::get('auth/user', function () {
+
+	if(auth()->check())
+		return response()->json([
+			'authUser' => auth()->user()
+		]);
+
+    return null;
+
+});
+
+Route::get('chat/{chat}', 'App\Http\Controllers\ChatController@show')->name('chat.show');
+
+Route::get('chat/with/{user}', 'App\Http\Controllers\ChatController@chat_with')->name('chat.with');
+
+Route::get('chat/{chat}/get_users', 'App\Http\Controllers\ChatController@get_users')->name('chat.get_users');
+
+Route::get('chat/{chat}/get_messages', 'App\Http\Controllers\ChatController@get_messages')->name('chat.get_messages');
+
+Route::post('message/sent', '\App\Http\Controllers\MessageController@sent')->name('message.sent');
+
+//Route::get('chat/with/{user}', 'App\Http\Controllers\ChatController@chat_with')->name('chat.with');
